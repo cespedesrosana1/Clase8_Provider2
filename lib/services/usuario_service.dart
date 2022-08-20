a11fae95a65ee86d4c4b6d9a58ea6af2ea9e1fc9
S684256 <rosana.cespedes@evertecinc.com>
2022-08-20 10:22:44 -0300

    Inicio ejercicio

---
+++ lib/services/usuario_service.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import '../models/usuario.dart';
+
+class UsuarioService with ChangeNotifier {
+  Usuario? _usuario;
+
+  Usuario? get usuario => _usuario;
+  set usuario(Usuario? user) {
+    _usuario = user;
+    notifyListeners();
+  }
+
+  bool get existeUsuario => (_usuario != null) ? true : false;
+
+  void removerUsuario() {
+    _usuario = null;
+    notifyListeners();
+  }
+}
